fcb92e7f6c109efde761ad3fefd0258ecba0cca4
Kirsi <kirsiito@metropolia.fi>
2022-04-20 15:24:11 +0300

    Testi

--- app/src/main/java/com/example/laakepaivakirja/MainActivity.java
+++ app/src/main/java/com/example/laakepaivakirja/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.laakepaivakirja;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    CalendarView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendar = findViewById(R.id.calendar);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String Date = dayOfMonth + "-" + (month + 1) + "-" + year;
+                    }
+});
     }
 }
---
+++ app/src/main/java/com/example/laakepaivakirja/ValikkoActivity.java
@@ -0,0 +1,14 @@
+package com.example.laakepaivakirja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ValikkoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_valikko);
+    }
+}
