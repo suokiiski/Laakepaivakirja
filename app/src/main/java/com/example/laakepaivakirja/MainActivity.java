dc0cc30212ff37c02f34ace131f4ab6019612502
Kirsi <kirsiito@metropolia.fi>
2022-04-21 14:56:31 +0300

    Testi

--- app/src/main/java/com/example/laakepaivakirja/MainActivity.java
+++ app/src/main/java/com/example/laakepaivakirja/MainActivity.java
@@ -2,26 +2,59 @@ package com.example.laakepaivakirja;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
+import com.example.laakepaivakirja.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     CalendarView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
-        calendar = findViewById(R.id.calendar);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new KotiFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new KotiFragment());
+                    break;
+                case R.id.lisaa:
+                    replaceFragment(new LisaaFragment());
+                    break;
+                case R.id.pisteet:
+                    replaceFragment(new MoreFragment());
+                    break;
+            }
+            return true;
+                    });
+
+/*        calendar = findViewById(R.id.calendar);
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 String Date = dayOfMonth + "-" + (month + 1) + "-" + year;
                     }
-});
+        });*/
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
     }
 }
