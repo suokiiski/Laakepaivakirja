9e470d36890ec1cd4001bfd7ff61452e5ce27676
Vera <finogenova.vera4@gmail.com>
2022-04-26 13:41:51 +0300

    Uusia juttuja

--- app/src/main/java/com/example/laakepaivakirja/LisaaActivity.java
+++ app/src/main/java/com/example/laakepaivakirja/LisaaActivity.java
@@ -5,15 +5,51 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.laakepaivakirja.databinding.ActivityMainBinding;
 
 public class LisaaActivity extends AppCompatActivity {
+    private EditText name, instructions, hour, min;
+    private TextView msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lisaa);
+        name = (EditText) findViewById(R.id.nameInput);
+        instructions = (EditText) findViewById(R.id.instructionInput);
+        hour = (EditText) findViewById(R.id.hoursInput);
+        min = (EditText) findViewById(R.id.minsInput);
+        msg = (TextView) findViewById(R.id.errorMsg);
+
+        Intent intent = getIntent();
+    }
+
+    public void sendData (View v) {
+        String strName = name.getText().toString();
+        String strInstructions = instructions.getText().toString();
+        String strMin = min.getText().toString();
+        String strHour = hour.getText().toString();
+
+        if (strName.isEmpty()) {
+            Toast.makeText(this, "Lisää lääkkeen nimi!", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (strInstructions.isEmpty()) {
+            Toast.makeText(this, "Lisää käyttöohjeet!", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (strHour.isEmpty() || strMin.isEmpty()) {
+            Toast.makeText(this, "Lisää aika!", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            MedicineSingleton.getInstance().addMedicine(strName, strInstructions, Integer.parseInt(strHour), Integer.parseInt(strMin));
+            Toast.makeText(this, "Lisätty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
     }
 }
--- app/src/main/java/com/example/laakepaivakirja/MainActivity.java
+++ app/src/main/java/com/example/laakepaivakirja/MainActivity.java
@@ -6,28 +6,46 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.laakepaivakirja.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
-    ActivityMainBinding binding;
     CalendarView calendar;
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-     calendar = findViewById(R.id.calendar);
+        lv = findViewById(R.id.medList);
+        lv.setAdapter(new ArrayAdapter<Medicine>(this, android.R.layout.simple_list_item_1, MedicineSingleton.getInstance().getMedicines()));
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
+        calendar = findViewById(R.id.calendar);
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 String Date = dayOfMonth + "-" + (month + 1) + "-" + year;
                     }
         });
+    }
+
+    public void addIsPressed (View v) {
+        Intent intent = new Intent(this, LisaaActivity.class);
+        startActivity(intent);
     }
 }
